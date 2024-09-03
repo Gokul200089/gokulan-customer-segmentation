05d5b26d9d079fa3a3aed6c1f25d043ae542f5d4
Gokul200089 <sivakumarangokulan@gmail.com>
2024-09-03 16:55:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ with st.expander("Data"):
   st.write("**Raw Data**")
   df = pd.read_csv("https://raw.githubusercontent.com/Gokul200089/gokulan-customer-segmentation/master/cleaned_data_no_index.csv")
   df
+
+  st.write('**X**')
+  X_raw = df.drop('Segmentation', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.Segmentation
+  y_raw
