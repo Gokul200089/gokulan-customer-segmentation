762a90ef5d4980534f98b551770d3885add351e7
Gokul200089 <sivakumarangokulan@gmail.com>
2024-09-03 16:53:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Customer Segmentation App')
 
 st.info('This app segments customer by their characteristics')
 
- with st.expander("Data"):
+with st.expander("Data"):
   st.write("**Raw Data**")
   df = pd.read_csv("https://raw.githubusercontent.com/Gokul200089/gokulan-customer-segmentation/master/cleaned_data_no_index.csv")
   df
