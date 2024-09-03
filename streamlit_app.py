575bcb129ac403e00a20be6607b3d3a5eea8d3cf
Gokul200089 <sivakumarangokulan@gmail.com>
2024-09-03 16:51:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.title('Customer Segmentation App')
 
 st.info('This app segments customer by their characteristics')
+
+ with st.expander("Data"):
+  st.write("**Raw Data**")
+  df = pd.read_csv("")
+  df
